31474a0f35147aa075316b5fba4bec2187442363
athomas227 <andrewthomas@pursuit.org>
2023-09-12 08:02:53 -0400

    finished lab

--- index.js
+++ index.js
@@ -4,7 +4,40 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+    let args = process.argv;
+    let operator = args[2];
+    let numbers = args.slice(3).map(Number);
+    let result = 0;
+
+    if (operator === "divide") {
+        return `Invalid operation: divide`
+    }
+    
+    if (args.length < 3) {
+        return `No operation provided...`;
+    }
+
+    if (args.length < 4) {
+        return `No numbers provided...`;
+    }
+
+    if (operator === 'plus') {
+        result = 0;
+        for (const num of numbers) {
+          result += num;
+        }
+    }
+    
+    if (operator === 'minus') {
+        result = numbers[0];
+        for (let i = 1; i < numbers.length; i++) {
+            result -= numbers[i];
+        }
+    }
+
+    return result
+}
 
 // Don't change anything below this line.
 module.exports = calculator;
