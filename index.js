1e25cbaafe5befba0c0f47f49e39439787f9c8e4
eaapursuit <elishaabochie-ansah@pursuit.org>
2023-09-25 16:20:34 -0400

    updated the files to let calculator compute

--- index.js
+++ index.js
@@ -4,7 +4,60 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+  const [arg1, arg2, calc, ...nums] = process.argv;
+  const isValid = validate(calc);
+  let num;
+
+  if (!isValid.status) {
+    return isValid.message;
+  }
+
+if(nums.length === 0){
+    return 'No numbers provided...'
+}
+
+  if (calc === "plus") {
+    num = add(nums);
+  }
+
+  if (calc === "minus") {
+    num = subtract(nums);
+  }
+console.log(num)
+  return num;
+}
+
+function add(nums) {
+  let output = 0;
+  for (let num of nums) {
+    output += Number(num);
+  }
+  return output;
+}
+
+function subtract(nums) {
+  let output = Number(nums[0]);
+  for (let i = 1; i < nums.length; i++) {
+    output -= Number(nums[i]);
+  }
+  return output;
+}
+
+function validate(calc) {
+  if (!calc) {
+    return { status: false, message: `No operation provided...` };
+  }
+
+  if (calc !== "plus" && calc !== "minus") {
+    return { status: false, message: `Invalid operation: ${process.argv[2]}` };
+  }
+  return { status: true };
+}
+
+console.log(calculator())
+
+
 
 // Don't change anything below this line.
 module.exports = calculator;
